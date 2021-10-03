b7218824aad9cb10350999517b371fe950e4740e
kunkun9811 <timochen77@gmail.com>
2021-10-02 23:47:06 -0700

    styling at board view not working for material UI idk why

--- src/pages/TodoPage/BoardView/Styles.ts
+++ src/pages/TodoPage/BoardView/Styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+import { makeStyles } from "@mui/styles"; // NOTE: 'makestyles' should be imported from this path
 
+/* styled components */
 export const BoardViewMainContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -39,6 +41,64 @@ export const BoardViewColumn = styled.div`
   background-color: grey;
 `;
 
+export const BoardViewColumnInfoContainer = styled.div`
+  display: grid;
+  grid-template-areas: "tag tag tag moreButton addButton";
+  height: 5%;
+  width: 100%;
+
+  background-color: green;
+`;
+
+export const BoardViewGroupNameContainer = styled.div`
+  grid-area: tag;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+
+  background-color: red;
+`;
+
+export const BoardViewGroupNameWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1px 2px;
+
+  background-color: skyblue;
+`;
+
+export const BoardViewGroupNameText = styled.p`
+  margin: 0;
+  color: black;
+`;
+
+export const BoardViewMoreButtonWrapper = styled.div`
+  grid-area: moreButton;
+  justify-self: center;
+  align-self: center;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: lightblue;
+`;
+
+export const BoardViewAddButtonWrapper = styled.div`
+  grid-area: addButton;
+  justify-self: center;
+  align-self: center;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: lightpink;
+`;
+
 export const BoardViewCardContainer = styled.div`
   height: 5%;
   width: 80%;
@@ -49,3 +109,12 @@ export const BoardViewCardContainer = styled.div`
   border: 1px solid black;
   border-radius: 7px;
 `;
+
+/* material UI themes */
+// TODO: not sure why this styling isn't working all of a sudden
+export const useStyles = makeStyles({
+  columnButton: {
+    height: 1,
+    width: 1,
+  },
+});
--- src/pages/TodoPage/BoardView/index.tsx
+++ src/pages/TodoPage/BoardView/index.tsx
@@ -1,32 +1,65 @@
 import React from "react";
 import { TableDataArray } from "../../../shared/models";
-import { BoardViewMainContainer, BoardViewColumn, BoardViewContentContainer, BoardViewCardContainer } from "./Styles";
+import {
+  BoardViewMainContainer,
+  BoardViewContentContainer,
+  BoardViewColumn,
+  BoardViewColumnInfoContainer,
+  BoardViewGroupNameContainer,
+  BoardViewGroupNameWrapper,
+  BoardViewGroupNameText,
+  BoardViewMoreButtonWrapper,
+  BoardViewAddButtonWrapper,
+  BoardViewCardContainer,
+  useStyles,
+} from "./Styles";
+
+import AddIcon from "@mui/icons-material/Add";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 // TODO: I don't believe react-table works with Kandan's Board View
 // NOTE: [10/1/2021] Going to implement my own Kandan's View by using classes
 const BoardView: React.FC<TableDataArray> = ({ data }) => {
+  // material UI custom styles
+  const classes = useStyles();
+
   return (
     <BoardViewMainContainer>
       {/* DEBUG: Mock view */}
-      {/* <BoardViewContentContainer>
+      <BoardViewContentContainer>
         <BoardViewColumn>
+          <BoardViewColumnInfoContainer>
+            <BoardViewGroupNameContainer>
+              <BoardViewGroupNameWrapper>
+                <BoardViewGroupNameText>Hello</BoardViewGroupNameText>
+              </BoardViewGroupNameWrapper>
+            </BoardViewGroupNameContainer>
+            <BoardViewMoreButtonWrapper>
+              <MoreHorizIcon className={classes.columnButton} />
+            </BoardViewMoreButtonWrapper>
+            <BoardViewAddButtonWrapper>
+              <AddIcon className={classes.columnButton} />
+            </BoardViewAddButtonWrapper>
+          </BoardViewColumnInfoContainer>
           <BoardViewCardContainer />
           <BoardViewCardContainer />
           <BoardViewCardContainer />
           <BoardViewCardContainer />
         </BoardViewColumn>
         <BoardViewColumn>
+          <BoardViewColumnInfoContainer></BoardViewColumnInfoContainer>
           <BoardViewCardContainer />
           <BoardViewCardContainer />
         </BoardViewColumn>
         <BoardViewColumn>
+          <BoardViewColumnInfoContainer></BoardViewColumnInfoContainer>
           <BoardViewCardContainer />
           <BoardViewCardContainer />
           <BoardViewCardContainer />
           <BoardViewCardContainer />
           <BoardViewCardContainer />
         </BoardViewColumn>
-      </BoardViewContentContainer> */}
+      </BoardViewContentContainer>
     </BoardViewMainContainer>
   );
 };
