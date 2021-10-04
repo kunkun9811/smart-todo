c952447f99cd2c050f026a5a9747d37e7eaa1a18
kunkun9811 <timochen77@gmail.com>
2021-10-03 17:05:16 -0700

    saved

--- src/pages/TodoPage/BoardView/index.tsx
+++ src/pages/TodoPage/BoardView/index.tsx
@@ -1,29 +1,10 @@
 import React from "react";
 import { TableDataArray } from "../../../shared/models";
-import AddIcon from "@mui/icons-material/Add";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import {
-  BoardViewMainContainer,
-  BoardViewContentContainer,
-  // BoardViewColumn,
-  // BoardViewColumnInfoContainer,
-  // BoardViedwColumnInfoWrapper,
-  // BoardViewGroupNameContainer,
-  // BoardViewGroupNameWrapper,
-  // BoardViewGroupNameText,
-  // BoardViewMoreButtonWrapper,
-  // BoardViewAddButtonWrapper,
-  // BoardViewCardContainer,
-  // useStyles,
-} from "./Styles";
+import { BoardViewMainContainer, BoardViewContentContainer } from "./Styles";
 import BoardColumn from "./BoardColumn";
 
-// TODO: I don't believe react-table works with Kandan's Board View
-// NOTE: [10/1/2021] Going to implement my own Kandan's View by using classes
+// TODO: [10/1/2021] Going to implement my own Kandan's View by using classes
 const BoardView: React.FC<TableDataArray> = ({ data }) => {
-  // material UI custom styles
-  // const classes = useStyles();
-
   return (
     <BoardViewMainContainer>
       {/* DEBUG: Mock view */}
