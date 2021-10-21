cd051a2a28bb287911f4d5fb9b1c3492782133d4
kunkun9811 <timochen77@gmail.com>
2021-10-21 00:07:14 -0700

    saved [10/21/2021]

--- src/state/action-creators/todosActionCreators.ts
+++ src/state/action-creators/todosActionCreators.ts
@@ -1,10 +1,10 @@
 /* Contain all the "Action Creators" for TodosReducer */
-import { TableDatum } from "../../shared/models";
+import { Datum } from "../../shared/models";
 import { TodosActionTypes } from "../action-types";
 import { Dispatch } from "redux"; // KEY: get the "Dispatch type" from redux
 import { TodosActions } from "../actions";
 
-export const AddTodo = (todo: TableDatum) => {
+export const AddTodo = (todo: Datum) => {
   return (dispatch: Dispatch<TodosActions>) => {
     dispatch({
       type: TodosActionTypes.ADD_TODO,
--- src/state/reducers/todosReducers.ts
+++ src/state/reducers/todosReducers.ts
@@ -1,12 +1,12 @@
 import { TodosActionTypes } from "../action-types";
 import { TodosActions } from "../actions";
-import { TableDataArray } from "../../shared/models/";
+import { DataArray } from "../../shared/models/";
 
 // NOTE: state's type is an array of TableData type => look at [models/tabledata.ts]
 // TODO: [9/29/2021] maybe change this to a class of "todo" in the future
-const initialState: TableDataArray["data"] = [];
+const initialState: DataArray["data"] = [];
 
-const reducer = (state = initialState, action: TodosActions): TableDataArray["data"] => {
+const reducer = (state = initialState, action: TodosActions): DataArray["data"] => {
   switch (action.type) {
     case TodosActionTypes.ADD_TODO:
       return [...state, action.payload];
